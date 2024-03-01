81da664723a39011bd9056c15bd0c50cf0569543
gasottorodrigues <ga.sotto.rodrigues@gmail.com>
2024-03-01 09:22:27 -0300

    feat: configura estrutura inicial do projeto
    Este commit estabelece a base sobre a qual a aplicação será construída, incluindo a configuração inicial do ambiente de desenvolvimento, estrutura de diretórios e arquivos essenciais para o início do desenvolvimento. Define também as dependências principais no arquivo pom.xml (para projetos Maven) ou build.gradle (para projetos Gradle), e implementa a classe principal da aplicação marcada com @SpringBootApplication para projetos Spring Boot.

---
+++ src/main/java/com/opus/squadscheme/SquadSchemeApplication.java
@@ -0,0 +1,13 @@
+package com.opus.squadscheme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadSchemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadSchemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opus/squadscheme/SquadSchemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opus.squadscheme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadSchemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
